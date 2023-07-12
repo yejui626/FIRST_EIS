557b99287149bd0a957e4c73b95d4b0b8535886f
Jokeryde <adamydee@gmail.com>
2023-07-12 17:38:57 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -182,7 +182,6 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
 	Route::put('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
 	Route::put('profile-password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
-
 	Route::get('profile/password', ['as' => 'profile.password.edit', 'uses' => 'App\Http\Controllers\ProfileController@editPassword']);
 	Route::put('profile/password', ['as' => 'profile.password.update', 'uses' => 'App\Http\Controllers\ProfileController@updatePassword']);
 });
