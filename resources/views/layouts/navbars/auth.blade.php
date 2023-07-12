85e7750e64f7c225f9224391e1cc48c9a660ff3b
Jokeryde <adamydee@gmail.com>
2023-07-12 18:38:24 +0800

    Update auth.blade.php

--- resources/views/layouts/navbars/auth.blade.php
+++ resources/views/layouts/navbars/auth.blade.php
@@ -10,15 +10,18 @@
         <a href="{{ route('sales.dashboard') }}" class="simple-text logo-normal">
             {{ __('TSK SYNERGY') }}
         </a>
-        @else
-        <a href="{{ route('page.index', 'dashboard') }}" class="simple-text logo-normal">
+        @elseif($role == 5)
+        <a href="{{url('customer')}}" class="simple-text logo-normal">
             {{ __('TSK SYNERGY') }}
-        </a>
-        @endif
+            @else
+            <a href="{{ route('page.index', 'dashboard') }}" class="simple-text logo-normal">
+                {{ __('TSK SYNERGY') }}
+            </a>
+            @endif
     </div>
     <div class="sidebar-wrapper">
         <ul class="nav">
-            
+
             @if($role == 1)
             <li class="{{ $elementActive == 'dashboard' ? 'active' : '' }}">
                 <a href="{{ route('sales.dashboard') }}">
@@ -34,17 +37,17 @@
                 </a>
             </li>
             @endif
-           
-            
-           
+
+
+
             <li class="{{ $elementActive == 'profile' ? 'active' : '' }}">
                 <a href="{{ route('profile.edit') }}">
                     <i class="nc-icon nc-paper"></i>
                     <p>{{ __('User Profile') }}</p>
                 </a>
             </li>
-           
-            
+
+
             <?php $role = Auth::user()->role; ?>
             @if($role == 3)
             <li class="{{ $elementActive == 'purchaserequest' ? 'active' : '' }}">
@@ -63,7 +66,7 @@
                 </a>
             </li>
             @endif
-            
+
             <?php $role = Auth::user()->role; ?>
             @if($role == 2)
             <li class="{{ $elementActive == 'supplier' ? 'active' : '' }}">
@@ -134,7 +137,7 @@
             </li>
             @endif
 
-            
+
 
             <?php $role = Auth::user()->role; ?>
             @if($role == 2)
@@ -169,7 +172,7 @@
             <?php $role = Auth::user()->role; ?>
             @if($role == 1)
             <li class="{{ $elementActive == 'sales-order-report' ? 'active' : '' }}">
-            <a href="{{ route('sales.monthly.report') }}">
+                <a href="{{ route('sales.monthly.report') }}">
                     <i class="nc-icon nc-single-copy-04"></i>
                     <p>{{ __('Report') }}</p>
                 </a>
@@ -179,8 +182,8 @@
             <?php $role = Auth::user()->role; ?>
             @if($role == 4)
             <li class="{{ $elementActive == 'logistic-detail' ? 'active' : '' }}">
-            <a href="{{ route('logistic.detail') }}">
-                <i class="nc-icon nc-delivery-fast"></i>
+                <a href="{{ route('logistic.detail') }}">
+                    <i class="nc-icon nc-delivery-fast"></i>
                     <p>{{ __('Logistic') }}</p>
                 </a>
             </li>
@@ -196,5 +199,6 @@
             </li>
             @endif
         </ul>
-    </div>
+           
+    </div>
 </div>
